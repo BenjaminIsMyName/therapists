6ef37736f81f586c00c7acb44db4f68aa857e9fa
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2023-05-25 17:34:17 +0300

    Basic hole in Packages

--- src/components/Package.tsx
+++ src/components/Package.tsx
@@ -1,11 +1,23 @@
-import { Noto_Sans } from 'next/font/google'
+import { Noto_Sans, Rubik_Dirt } from 'next/font/google'
 const notoSans = Noto_Sans({ subsets: ['latin'], weight: '900' })
+const rubik = Rubik_Dirt({ subsets: ['hebrew'], weight: '400' })
+
 export default function Package({ name }: { name: string }) {
     return (
         <div
-            className={`bg-white/30 w-52 h-52 rounded-2xl flex flex-col gap-4 items-center justify-center ${notoSans.className}`}
+            className={`w-52 h-52 flex flex-col gap-4 items-center justify-center ${notoSans.className}
+            relative 
+            before:hole before:content-[''] before:bg-white/20 before:backdrop-blur-md 
+            before:absolute before:inset-0 before:z-[-1] before:rounded-2xl
+            `}
         >
-            <span>{name}</span>
+            <span
+                className={`font-extrabold text-transparent bg-clip-text 
+                bg-gradient-to-r from-purple-100 to-white text-xl ${rubik.className}
+                `}
+            >
+                {name}
+            </span>
 
             <span className="font-light text-sm">הצטרף כדי לקבל הצעת מחיר</span>
         </div>
