0c4dde19da5168425549da51734994c3ec87eb6c
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2023-07-09 00:31:06 +0300

    Deleted temporary code

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,8 @@ module.exports = {
         extend: {
             keyframes: {
                 'show-up': {
-                    '0%': { opacity: '0', transform: 'translateY(20px)' },
-                    '100%': { opacity: '0.25', transform: 'translateY(0)' },
+                    '0%': { opacity: '0' },
+                    '100%': { opacity: '0.25' },
                 },
             },
             backgroundImage: {
