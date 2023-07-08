37eca429629e4a27c117d3b847d3538804e28669
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2023-07-08 23:11:22 +0300

    Fixed WelcomeSection content

--- src/app/WelcomeSection.tsx
+++ src/app/WelcomeSection.tsx
@@ -12,7 +12,7 @@ export default function WelcomeSection() {
         <Section>
             <div className="h-screen flex items-center justify-center text-white">
                 <div>
-                    <div className="flex gap-4 items-baseline justify-center">
+                    <div className="flex sm:gap-4 items-baseline justify-center">
                         <GiOnTarget className="w-10 h-10 md:w-24 md:h-24 self-end" />
                         <h1
                             className={`${notoSans.className} text-4xl md:text-9xl text-center tracking-widest`}
@@ -29,14 +29,14 @@ export default function WelcomeSection() {
                     >
                         אפשר גם אחרת
                     </h2>
-
-                    <p
-                        className={`${notoSansRegular.className} text-md md:text-2xl text-center px-8 absolute bottom-14 left-1/2 -translate-x-1/2`}
-                    >
-                        טיפול CBT קוגנטיבי-התנהגותי ושיטות פורצות דרך ייחודיות
-                        ברוח היהדות שיתנו לכם מענה למגוון בעיות
-                    </p>
                 </div>
+                <p
+                    className={`${notoSansRegular.className} text-md md:text-2xl text-center absolute bottom-14 left-1/2 -translate-x-1/2
+                    w-11/12 md:w-1/2`}
+                >
+                    טיפול CBT קוגנטיבי-התנהגותי ושיטות פורצות דרך ייחודיות ברוח
+                    היהדות שיתנו לכם מענה למגוון בעיות
+                </p>
             </div>
         </Section>
     )
