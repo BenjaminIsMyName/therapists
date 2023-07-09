f20b4ed1dc84d08cf0e5b4a4252e4226ad5a268b
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2023-07-09 21:14:40 +0300

    List animation

--- src/app/AdvantagesSection.tsx
+++ src/app/AdvantagesSection.tsx
@@ -1,42 +1,35 @@
+'use client'
+
 import Advantage from '@/components/Advantage'
 import Section from '@/components/Section'
+import { useInView } from 'framer-motion'
 import { BsCheck2All } from 'react-icons/bs'
+import { useRef } from 'react'
 
 export default function AdvantagesSection() {
+    const advantages = [
+        'חרדות',
+        'פוסט-טראומה',
+        'דיכאון',
+        'כפייתיות',
+        'ביטחון עצמי',
+        'מחשבות טורדניות',
+        'ייעוץ זוגי',
+    ]
+    const ref = useRef(null)
+    const isInView = useInView(ref, { once: true })
     return (
         <Section>
             <div className="min-h-screen flex items-center justify-center text-white flex-col gap-10">
                 <div className="text-5xl">טיפול ב...</div>
-                <div className="flex flex-col gap-4 px-9">
-                    <Advantage>
-                        <BsCheck2All />
-                        <span> חרדות</span>
-                    </Advantage>
-
-                    <Advantage>
-                        <BsCheck2All />
-                        <span> פוסט-טראומה</span>
-                    </Advantage>
-                    <Advantage>
-                        <BsCheck2All />
-                        <span>דיכאון</span>
-                    </Advantage>
-                    <Advantage>
-                        <BsCheck2All />
-                        <span>כפייתיות</span>
-                    </Advantage>
-                    <Advantage>
-                        <BsCheck2All />
-                        <span>ביטחון עצמי</span>
-                    </Advantage>
-                    <Advantage>
-                        <BsCheck2All />
-                        <span>מחשבות טורדניות </span>
-                    </Advantage>
-                    <Advantage>
-                        <BsCheck2All />
-                        <span>ייעוץ זוגי </span>
-                    </Advantage>
+                <div ref={ref} className="flex flex-col gap-4 px-9">
+                    {isInView &&
+                        advantages.map((advantage, index) => (
+                            <Advantage key={advantage} delay={index}>
+                                <BsCheck2All />
+                                <span>{advantage}</span>
+                            </Advantage>
+                        ))}
                 </div>
             </div>
         </Section>
--- src/components/Advantage.tsx
+++ src/components/Advantage.tsx
@@ -1,3 +1,20 @@
-export default function Advantage({ children }: { children: React.ReactNode }) {
-    return <div className="flex gap-4 items-center text-xl">{children}</div>
+import { motion as m } from 'framer-motion'
+
+export default function Advantage({
+    children,
+    delay,
+}: {
+    children: React.ReactNode
+    delay: number
+}) {
+    return (
+        <m.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: delay * 0.3 }}
+            className="flex gap-4 items-center text-xl"
+        >
+            {children}
+        </m.div>
+    )
 }
